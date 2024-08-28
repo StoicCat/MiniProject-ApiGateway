de2cba6ef1a18ea051cd2768f235809d1acb188b
torotoro21 <anggaprihantoro3@gmail.com>
2024-08-28 13:43:44 +0700

    initial commit

---
+++ Dummy Service/OlShop/src/test/java/com/miniProject/OlShop/OlShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniProject.OlShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
