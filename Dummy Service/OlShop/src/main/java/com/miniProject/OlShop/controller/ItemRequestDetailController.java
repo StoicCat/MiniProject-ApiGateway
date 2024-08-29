921255a07bbbef5d37bafe19823d60c6157f58a7
torotoro21 <anggaprihantoro3@gmail.com>
2024-08-29 09:48:54 +0700

    adding some controller

---
+++ Dummy Service/OlShop/src/main/java/com/miniProject/OlShop/controller/ItemRequestDetailController.java
@@ -0,0 +1,16 @@
+package com.miniProject.OlShop.controller;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miniProject.OlShop.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("item-request-detail")
+@RequiredArgsConstructor
+public class ItemRequestDetailController {
+
+}
